2285c43664e314d3eecd21e39a52689da8801aef
luyuh <774964107@qq.com>
2021-05-25 11:41:52 +0800

    add ast and some test

---
+++ ast.py
@@ -0,0 +1,167 @@
+"""
+Lua 5.1 Reference Manual
+chunk ::= {stat [`;´]} [laststat[`;´]]
+
+block ::= chunk
+
+stat ::=  varlist1 `=´ explist1  |
+         functioncall  |
+         do block end  |
+         while exp do block end  |
+         repeat block until exp  |
+         if exp then block {elseif exp then block} [else block] end  |
+         for Name `=´ exp `,´ exp [`,´ exp] do block end  |
+         for namelist in explist1 do block end  |
+         function funcname funcbody  |
+         local function Name funcbody  |
+         local namelist [`=´ explist1]
+
+laststat ::= return [explist1]  |  break
+
+funcname ::= Name {`.´ Name} [`:´ Name]
+
+varlist1 ::= var {`,´ var}
+
+var ::=  Name  |  prefixexp `[´ exp `]´  |  prefixexp `.´ Name
+
+namelist ::= Name {`,´ Name}
+
+explist1 ::= {exp `,´} exp
+
+exp ::=  nil  |  false  |  true  |  Number  |  String  |  `...´  |
+         function  |  prefixexp  |  tableconstructor  |  exp binop exp  |  unop exp
+
+prefixexp ::= var  |  functioncall  |  `(´ exp `)´
+
+functioncall ::=  prefixexp args  |  prefixexp `:´ Name args
+
+args ::=  `(´ [explist1] `)´  |  tableconstructor  |  String
+
+function ::= function funcbody
+
+funcbody ::= `(´ [parlist1] `)´ block end
+
+parlist1 ::= namelist [`,´ `...´]  |  `...´
+
+tableconstructor ::= `{´ [fieldlist] `}´
+
+fieldlist ::= field {fieldsep field} [fieldsep]
+
+field ::= `[´ exp `]´ `=´ exp  |  Name `=´ exp  |  exp
+
+fieldsep ::= `,´  |  `;´
+
+binop ::= `+´  |  `-´  |  `*´  |  `/´  |  `^´  |  `%´  |  `..´  |
+         `<´  |  `<=´  |  `>´  |  `>=´  |  `==´  |  `~=´  |
+         and  |  or
+
+unop ::= `-´  |  not  |  `#´
+"""
+
+
+class Stmt:
+    pass
+
+    # exp: := nil | false | true | Number | String | `...´ |
+    # function | prefixexp | tableconstructor | exp
+    # binop
+    # exp | unop
+    # exp
+
+
+class Expr:
+    CONSTANT = 1
+    PREFIX = 2
+    BINOP = 3
+    UNOP = 4
+
+    def __init__(self, t, left=None, right=None):
+        self.type = t
+        self.left = left
+        self.right = right
+
+
+class BinOp:
+    ADD = 1
+    SUB = 2
+    MUL = 3
+    DIV = 4
+    XOR = 5
+    MOD = 6
+    LT = 7
+    LTE = 8
+    GT = 9
+    GTE = 10
+    EQ = 11
+    CONCAT = 12
+    ASSIGN_XOR = 13
+
+    def __init__(self, op, left: Expr, right: Expr):
+        self.operator = op
+        self.left = left
+        self.right = right
+
+
+class Constant:
+    NIL = 1
+    FALSE = 2
+    TRUE = 3
+    NUMBER = 4
+    STRING = 5
+
+    def __init__(self, t, value=None):
+        self.type = t
+        self.value = value
+
+
+def gen_const_nil() -> Constant:
+    return Constant(Constant.NIL)
+
+
+def gen_const_false() -> Constant:
+    return Constant(Constant.FALSE, False)
+
+
+def gen_const_true() -> Constant:
+    return Constant(Constant.FALSE, True)
+
+
+def gen_const_string(s) -> Constant:
+    return Constant(Constant.STRING, s)
+
+
+def gen_const_number(n) -> Constant:
+    return Constant(Constant.NUMBER, n)
+
+
+def gen_expr_const(c:  Constant) -> Expr:
+    return Expr(Expr.CONSTANT, c)
+
+
+def gen_expr_prefix(c):
+    return Expr(Expr.PREFIX, c)
+
+
+def gen_expr_binop(left: Constant, right: Constant) -> Expr:
+    return Expr(Expr.BINOP, left, right)
+
+
+def gen_expr_unop(left: Expr) -> Expr:
+    return Expr(Expr.UNOP, left)
+
+
+
+
+class AddStmt(Stmt):
+    pass
+
+class SubStmt(Stmt):
+    pass
+
+class MulStmt(Stmt):
+    pass
+
+class DivStmt(Stmt):
+    pass
+
+
---
+++ tests/test_ast.py
@@ -0,0 +1,30 @@
+
+import unittest
+from ast import *
+
+
+class TestAst(unittest.TestCase):
+    def test_constant(self):
+        c1 = gen_const_nil()
+        c2 = gen_const_string('string const')
+        c3 = gen_const_number(1234)
+        c4 = gen_const_true()
+        c5 = gen_const_false()
+
+        print(c1)
+        print(c2)
+        print(c3)
+        print(c4)
+        print(c5)
+
+    def test_binop(self):
+        left = gen_expr_const(gen_const_true())
+        right = gen_expr_const(gen_const_false())
+
+        binop = gen_expr_binop(left, right)
+
+        print(binop)
+
+
+if __name__ == '__main__':
+    unittest.main()
